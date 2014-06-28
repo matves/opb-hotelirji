2ae8bd06d1939719400a4efd723e624ee1a9f97c
Mateja Vesel <vesel.mateja@gmail.com>
2014-06-28 18:45:52 +0200

    Popravki main, termin, sheme (dodan pogled), hoteli (glej mail).

--- shema.sql
+++ shema.sql
@@ -35,3 +35,15 @@ PRIMARY KEY (soba, zacetek, konec)
 CREATE VIEW rezervirane_sobe AS 
 SELECT sid, oseba, cena AS cena_teden_brez_popusta, kapaciteta, tip, zacetek, konec FROM soba JOIN termin ON soba.sid=termin.soba
 
+CREATE VIEW pregled_rezervacij AS
+SELECT oseba.oid, oseba.ime, oseba.priimek, oseba.tel_st, termin.soba, soba.kapaciteta, soba.tip, termin.zacetek, termin.konec,  
+round((soba.cena*(count(dnevi.serija))+1.2*soba.cena*(termin.konec-termin.zacetek-count(dnevi.serija)))*(100-termin.popust)*1./100.,2) as znesek 
+FROM oseba LEFT JOIN termin ON termin.oseba=oseba.oid 
+JOIN soba ON termin.soba=soba.sid
+LEFT JOIN 
+(SELECT oseba, zacetek, generate_series(zacetek::date, (konec-1)::date, '1 day') as serija FROM termin days) as dnevi
+ON termin.zacetek=dnevi.zacetek AND termin.oseba=dnevi.oseba
+WHERE extract('dow' from serija) not in (5,6)
+GROUP BY oseba.oid,oseba.ime,oseba.priimek, oseba.tel_st,termin.soba, soba.kapaciteta, soba.tip,soba.cena,termin.zacetek,termin.popust, termin.konec
+ORDER BY termin.zacetek;
+
